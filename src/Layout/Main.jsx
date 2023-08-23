932f0f22cae0f533420b2759b219c0eb563623de
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-08-23 18:46:41 +0600

    Footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
 
 const Main = () => {
     return (
-        <div>
-            hello
+        <div className="max-w-screen-xl mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
